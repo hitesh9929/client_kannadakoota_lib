364df07b2d1da0ff887836b4995a988f1affaf1a
hitesh9929 <hiteshabishnoi@gmail.com>
2023-03-26 20:08:02 +0530

    added view available books

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 // import logo from './logo.svg';
-import './App.css';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import Landing from './Components/Landing';
--- src/Components/AvailBooks.js
+++ src/Components/AvailBooks.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import { useState ,useEffect} from 'react';
+import Axios from 'axios';
+
 
 export default function AvailBooks() {
+  const [mydata,setmydata]=useState([]);
+
+
+  const getlist=()=>{
+    var url=`${process.env.REACT_APP_HOST}/available`;
+Axios.get(url)
+.then(res=>{
+    setmydata(res.data);
+console.log(mydata);
+
+}).catch(err=>alert(err))
+  }
+
+  useEffect(()=>{
+    getlist();
+
+  },[]);
+  
   return (
-    <div>
-        
-    <h1 className='h1'> Coming Soon...</h1></div>
+    <div>  <table className="table table-sm table-dark">
+    <thead>
+      <tr>
+        <th >      </th>
+
+        <th >ಪುಸ್ತಕದ ಹೆಸರು</th>
+      </tr>
+    </thead>
+    <tbody>
+    {mydata.map((item,index)=>(
+      <tr key={index}>
+              <th>{index+1}</th>
+              <td>{item.name}</td>
+            </tr>
+      ))}
+    </tbody>
+  </table></div>
   )
 }
